204592999bd67bfdfaa85f11135ffd8fe39676f8
MD.SHAHZAD HUSSAIN RAYIED <rayiedhussain8@gmail.com>
2026-02-04 01:31:40 +0600

    client side and server side rendering introduced

---
+++ 01-nextjs-demo/app/components/Hello.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+const Hello = () => {
+  console.log("I'm a client component");
+  return (
+    <div>hello</div>
+  )
+}
+
+export default Hello
--- 01-nextjs-demo/app/page.tsx
+++ 01-nextjs-demo/app/page.tsx
@@ -1,10 +1,15 @@
+import Hello from "./components/Hello";
 
 const page = () => {
+  console.log("What type of a component a I?");
   return (
-    <div className="text-5xl underline">
+    <main>
+      <div className="text-5xl underline">
       <h1>Welcome to Next.js!</h1>
     </div>
+    <Hello/>
+    </main>
   )
 }
 
-export default page
+export default page;
