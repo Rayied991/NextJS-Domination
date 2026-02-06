33537f7cbd2508c65fa907775b9ce4a51081d331
MD.SHAHZAD HUSSAIN RAYIED <rayiedhussain8@gmail.com>
2026-02-06 20:49:22 +0600

    initialized project and updated the fonts

---
+++ Project/01-devevents/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Martian_Mono, Schibsted_Grotesk } from "next/font/google";
+import "./globals.css";
+
+const schibstedGrotesk = Schibsted_Grotesk({
+  variable: "--font-schibsted-grotesk",
+  subsets: ["latin"],
+});
+
+const martianMono = Martian_Mono({
+  variable: "--font-martian-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "DevEvent",
+  description: "The Hub for Every Dev Event You Mustn't Miss",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
